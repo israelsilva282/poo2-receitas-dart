b8809fc21658bd909d71630689f00ef8fd3fdf35
israelsilva282 <israelcostas@hotmail.com>
2023-05-25 07:38:40 -0300

    Adicionado botões com icones

--- lib/main.dart
+++ lib/main.dart
@@ -14,19 +14,21 @@ class MyApp extends StatelessWidget {
         title: 'Primeiro App',
         theme: ThemeData(primarySwatch: Colors.purple),
         home: Scaffold(
-            appBar: AppBar(title: const Text("Meu app")),
-            body: const Center(
-                child: Column(children: [
-              Text(
-                "Apenas começando...",
-                style: TextStyle(fontWeight: FontWeight.bold),
-              ),
-              Text("No meio..."),
-              Text("Terminando...")
-            ])),
-            bottomNavigationBar: ElevatedButton(
-              child: const Text("Clique aqui"),
-              onPressed: () => print("Clicou"),
-            )));
+          appBar: AppBar(title: const Text("Meu app")),
+          body: const Center(
+              child: Column(children: [
+            Text(
+              "Apenas começando...",
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+            Text("No meio..."),
+            Text("Terminando...")
+          ])),
+          bottomNavigationBar: BottomNavigationBar(items: const [
+            BottomNavigationBarItem(
+                label: "Favorito", icon: Icon(Icons.favorite)),
+            BottomNavigationBarItem(label: "Fechar", icon: Icon(Icons.close))
+          ]),
+        ));
   }
 }
