1d23afcaa8794541aaf2a3b9052e559e51639ea2
israelsilva282 <israelcostas@hotmail.com>
2023-05-29 16:07:04 -0300

    Questões 1 e 2

--- lib/main.dart
+++ lib/main.dart
@@ -1,34 +1,57 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+class NewNavBar extends StatelessWidget {
+  const NewNavBar({super.key});
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  void botaoFoiTocado(int index) {
+    print("Tocaram no botão $index");
+  }
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        title: 'Primeiro App',
-        theme: ThemeData(primarySwatch: Colors.purple),
-        home: Scaffold(
-          appBar: AppBar(title: const Text("Meu app")),
-          body: const Center(
-              child: Column(children: [
-            Text(
-              "Apenas começando...",
-              style: TextStyle(fontWeight: FontWeight.bold),
-            ),
-            Text("No meio..."),
-            Text("Terminando...")
-          ])),
-          bottomNavigationBar: BottomNavigationBar(items: const [
-            BottomNavigationBarItem(
-                label: "Favorito", icon: Icon(Icons.favorite)),
-            BottomNavigationBarItem(label: "Fechar", icon: Icon(Icons.close))
-          ]),
-        ));
+    return BottomNavigationBar(onTap: botaoFoiTocado, items: const [
+      BottomNavigationBarItem(
+        label: "Cafés",
+        icon: Icon(Icons.coffee_outlined),
+      ),
+      BottomNavigationBarItem(
+          label: "Cervejas", icon: Icon(Icons.local_drink_outlined)),
+      BottomNavigationBarItem(label: "Nações", icon: Icon(Icons.flag_outlined))
+    ]);
   }
+}
+
+class NewBody extends StatelessWidget {
+  const NewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text("Israel"),
+    );
+  }
+}
+
+class NewAppBar extends StatelessWidget implements PreferredSizeWidget {
+  const NewAppBar({super.key});
+
+  @override
+  Size get preferredSize => const Size.fromHeight(kToolbarHeight);
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(title: const Text("Receita 2"));
+  }
+}
+
+void main() {
+  MaterialApp app = MaterialApp(
+      theme: ThemeData(primarySwatch: Colors.deepPurple),
+      home: const Scaffold(
+        appBar: NewAppBar(),
+        body: NewBody(),
+        bottomNavigationBar: NewNavBar(),
+      ));
+
+  runApp(app);
 }
